46ef8df6e33d5e0f5f087141d44a55bf9741e662
Vitor-Viana <vitorviana@id.uff.br>
2023-02-24 12:18:02 -0300

    Configurando templates
    Criando pagina de listagem.

--- core/models.py
+++ core/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Anotacao (models.Model):
-    titulo = models.CharField (max_length=100)
-    descricao = models.TextField (blank=True, null=True)
-    data_hora = models.DateTimeField ()
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_hora = models.DateTimeField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_data_hora(self):
+        return self.data_hora.strftime('%d/%m/%Y %H:%M Hrs')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.models import Anotacao
 
 # Create your views here.
+
+def agenda(request):
+    usuario = request.user
+    anotacoes = Anotacao.objects.filter(usuario=usuario)
+    response = {'anotacoes':anotacoes, 'usuario':usuario}
+    return render(request, 'agenda.html', response)
